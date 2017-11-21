c418a9a734fff322af123a0ab1a06ed194b6febf
Jeff Dederick <Xpressivmotion@gmail.com>
2017-11-21 17:13:40 -0500

    working on redis pairs

--- index.js
+++ index.js
@@ -6,18 +6,49 @@ const redis = require('redis');
 const redisClient = redis.createClient();
 const shortener = require('./link_shortener');
 const bodyparse = require('body-parser');
+const handlebars = require('express-handlebars');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(
+  '/socket.io',
+  express.static(__dirname + 'node_modules/socket.io-client/dist/')
+);
+
+const hbs = handlebars.create({
+  defaultLayout: 'main'
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(bodyparse());
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.render('index');
 });
 
 app.post('/', (req, res, next) => {
   let url = req.body.original_url;
   let short_url = shortener(url);
   redisClient.set(short_url[0], short_url[1], err => {
-    err ? next(err) : res.redirect('/');
+    if (err) {
+      next(err);
+    } else {
+      let redKeys = redisClient.keys('*', (err, keys) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(keys);
+          let originalUrls = [];
+          for (let i = 0; i < keys.length; i++) {
+            originalUrls.push(keys[i]);
+          }
+          console.log(originalUrls);
+          return keys;
+        }
+      });
+    }
   });
 });
 
@@ -31,4 +62,16 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: err });
 });
 
-app.listen(3000);
+let count = 0;
+
+io.on('connection', client => {
+  client.emit('new count', count);
+
+  //increment
+  client.on('increment', () => {
+    count++;
+    io.emit('new count', count); //emit count to all clients connected to this socket
+  });
+});
+
+server.listen(3000);
